10675733e5e8932960ac3ffa6ecec41505a8cb34
hadarasher <129529089+hadarasher@users.noreply.github.com>
2024-04-06 00:29:30 +0300

    upload recipe from gallery

--- app/src/main/java/com/example/recipehive/fragments/UploadRecipeFragment.java
+++ app/src/main/java/com/example/recipehive/fragments/UploadRecipeFragment.java
@@ -1,66 +1,117 @@
 package com.example.recipehive.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.example.recipehive.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UploadRecipeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UploadRecipeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UploadRecipeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UploadRecipeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UploadRecipeFragment newInstance(String param1, String param2) {
+    String recipeTypeSpinnerString;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    private Uri imageUri;
+    ImageView recipeImageView;
+    public static UploadRecipeFragment newInstance() {
         UploadRecipeFragment fragment = new UploadRecipeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_recipe, container, false);
+        View view = inflater.inflate(R.layout.fragment_upload_recipe, container, false);
+
+        EditText recipeNameEdit=view.findViewById(R.id.recipeNameEditText);
+        Button uploadFromGalleryBtn=view.findViewById(R.id.uploadFromGalleryBtn);
+        Button pictureWithCameraBtn=view.findViewById(R.id.pictureWithCameraBtn);
+        Spinner recipeTypeSpinner=view.findViewById(R.id.recipeTypeSpinner);
+        recipeImageView=view.findViewById(R.id.recipeImageView);
+        Button saveRecipeBtn=view.findViewById(R.id.saveRecipeBtn);
+
+        // Set spinner values
+        ArrayAdapter arrayAdapter=new ArrayAdapter(getContext(), R.drawable.spinner_item,getResources().getStringArray(R.array.recipe_types));
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        recipeTypeSpinner.setAdapter(arrayAdapter);
+        recipeTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                recipeTypeSpinnerString = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //set buttons' listeners
+        uploadFromGalleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SelectImage();
+            }
+        });
+
+        pictureWithCameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
+
+
+
+        return view;
     }
+
+    private void SelectImage() {
+        // Defining Implicit Intent to mobile gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            recipeImageView.setImageURI(imageUri);
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            recipeImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
